fcc1ae87ed98aaf0258d50deb240f75313909ea2
Miriam Villeda <102434406+miryv@users.noreply.github.com>
2023-11-04 16:29:17 -0600

    Add files via upload

---
+++ asilo/serializers.py
@@ -0,0 +1,21 @@
+# serializers.py
+from rest_framework import serializers
+from .models import Doctor, Paciente, Cita
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = "__all__"
+
+
+class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paciente
+        fields = "__all__"
+
+
+class CitaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cita
+        fields = "__all__"
